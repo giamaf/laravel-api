704bf759b226fe6c0dbccd08cc0711af00aa8fdc
giamaf <gianluca.maffucci@hotmail.com>
2024-03-28 17:24:27 +0100

    updated api's query to send project type

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
     public function index()
     {
         //! Preparo la query per mandare giù tutti i progetti
-        $projects = Project::all();
+        $projects = Project::whereIsCompleted(true)->latest()->with('type')->get();
 
         //! Restituisco un json da utilizzare nel front-end
         return response()->json($projects);
