82a7eaa397b91ea0da14d93c713de56ca6b89552
giamaf <gianluca.maffucci@hotmail.com>
2024-04-03 17:17:09 +0200

    make TechnologyProject route + controller

---
+++ app/Http/Controllers/Api/TechnologyProjectController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TechnologyProjectController extends Controller
+{
+    public function __invoke()
+    {
+        return 'Tech Api';
+    }
+}
