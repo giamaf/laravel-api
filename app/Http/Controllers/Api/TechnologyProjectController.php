987a179810c8d71c91666f0e912aeb759682488e
giamaf <gianluca.maffucci@hotmail.com>
2024-04-03 17:56:32 +0200

    make action to send projects based on technology

--- app/Http/Controllers/Api/TechnologyProjectController.php
+++ app/Http/Controllers/Api/TechnologyProjectController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class TechnologyProjectController extends Controller
 {
-    public function __invoke()
+    public function __invoke(string $slug)
     {
-        return 'Tech Api';
+        $technology = Technology::whereSlug($slug)->first();
+
+        if (!$technology) return response(null, 404);
+
+        $projects = Project::whereHas('technologies', function ($query) use ($technology) {
+            $query->where('technologies.id', $technology->id)->whereIsCompleted(true);
+        })->get();
+
+        return response()->json(['projects' => $projects, 'label' => $technology->label]);
     }
 }
