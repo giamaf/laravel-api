fe33975beda3b4e97ef016cca468a0beafe1748e
giamaf <gianluca.maffucci@hotmail.com>
2024-04-03 17:54:33 +0200

    make action to send data

--- app/Http/Controllers/Api/TypeProjectController.php
+++ app/Http/Controllers/Api/TypeProjectController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class TypeProjectController extends Controller
 {
-    public function __invoke()
+    public function __invoke(string $slug)
     {
-        return 'ciao';
+        $type = Type::whereSlug($slug)->first();
+
+        if (!$type) return response(null, 404);
+
+        $projects = Project::whereIsCompleted(true)->whereTypeId($type->id)->get();
+
+        return response()->json(['projects' => $projects, 'label' => $type->label]);
     }
 }
